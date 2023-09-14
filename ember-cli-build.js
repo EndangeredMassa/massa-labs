f389a58c525fe852024150eea4302d71a85fe4e9
Sean Massa <endangeredmassa@gmail.com>
2023-09-13 22:46:35 -0500

    try fixing css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    minifyCSS: {
+      options: { processImport: true },
+    },
     prember: {
       urls: [
         '/',
