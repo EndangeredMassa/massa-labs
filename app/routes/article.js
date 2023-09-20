6ff4f15143eabaa5ca269acf85064788f4f3c28d
Sean Massa <endangeredmassa@gmail.com>
2023-09-20 01:31:35 -0500

    fix prerender

--- app/routes/article.js
+++ app/routes/article.js
@@ -2,15 +2,13 @@ import Route from '@ember/routing/route';
 import articles from '../article-data';
 
 export default class ArticlesArticleRoute extends Route {
-  slug;
-
   beforeModel(transition) {
     // set the proper template
-    this.slug = transition.intent.url.replace('/articles/', '');
-    this.templateName = `articles.${this.slug}`;
+    let slug = transition.intent.url.replace('/articles/', '');
+    this.templateName = `articles.${slug}`;
   }
 
-  model() {
+  model(params) {
     let article = articles.find(a => a.slug === params.slug)
     return article;
   }
