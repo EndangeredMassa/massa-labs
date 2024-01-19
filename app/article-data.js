ace0ab9fb0015cc922efe934282a038fdd286955
Sean Massa <endangeredmassa@gmail.com>
2024-01-18 21:33:35 -0600

    add tags

--- app/article-data.js
+++ app/article-data.js
@@ -5,6 +5,7 @@ let articles =
     "published": true,
     "title": "Theme Pact: Goal Setting Framework with no Goals",
     "date": "2023-02-02",
+    "tags": ["productivity"],
     "description": "Pulling from several sources, my framework for goal setting includes:<br><ul><li><b>Theme:</b> a guiding light, a default decision</li><li><b>PACT Goals:</b> specific personal outputs I commit to</li><li><b>Habits:</b> systems for making sure I achieve those outputs</li></ul>"
   },
   {
@@ -12,6 +13,7 @@ let articles =
     "published": true,
     "title": "Conventional Comments: Streamlining Feedback",
     "date": "2022-03-15",
+    "tags": ["productivity"],
     "description": "How I use Conventional Comments to make it clear (1) what kind of comment it is, (2) what specifically I want, and (3) how important it is that my comment is resolved."
   },
   {
@@ -19,6 +21,7 @@ let articles =
     "published": true,
     "title": "Detangling the Code Review: Questions, Preferences, Suggestions, Conventions, Requirements",
     "date": "2022-03-14",
+    "tags": ["productivity"],
     "description": "Teams can have more effective code reviews if they default to approval and have a framework for delivering and managing feedback."
   },
   {
@@ -26,6 +29,7 @@ let articles =
     "published": true,
     "title": "Detangling the Standup: Status Reports, Team Planning, Context Sharing, and Community Building",
     "date": "2022-01-04",
+    "tags": ["productivity"],
     "description": "Standups are often applied without understanding the problem they are solving. This leads to:<br><ol><li>Not clearly solving a specific problem</li><li>Not solving a problem with the best solution</li><li>A daily interruption to deep work</li></ol>What follows is a pragmatic and ideal approach to resolving this situation. The pragmatic approach is to understand so you can excel within that system. The ideal solution tries to change the system to better serve the team."
   },
   {
@@ -33,6 +37,7 @@ let articles =
     "published": true,
     "title": "Detangling the Manager: Supervisor, Team Lead, Mentor",
     "date": "2021-11-18",
+    "tags": ["organizational-development"],
     "description": "The term Manager means a lot of different things to different companies. It can include Project Manager, Product Manager, Technical Lead, Team Lead, Mentor, Coach, Supervisor, as well as Individual Contributor. Most often, in my experience, a manager is responsible for at least these roles: Supervisor, Mentor, and Team Lead.<br><br>There is great value in having those roles played by separate people."
   },
   {
@@ -40,6 +45,7 @@ let articles =
     "published": true,
     "title": "A Developer's Guide to Better Accessibility",
     "date": "2017-12-07",
+    "tags": ["accessibility"],
     "description": "Melanie Sumner and I wrote about the \"Six Pillars of Accessibility\" as a structured guide for developers to move the needle on accessibility."
   },
   {
@@ -47,6 +53,7 @@ let articles =
     "published": true,
     "title": "An Incremental Approach to Linting to Your Projects",
     "date": "2017-08-08",
+    "tags": ["productivity"],
     "description": "I have gone through the process of adding linting to existing, large projects a couple of times. I've learned some lessons about how to approach this so that it's not disruptive to the team doing other work. Follow these steps to get robust linting into your project without pulling team velocity to a halt!"
   }
 ]
