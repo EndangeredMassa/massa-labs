c3e520a842366ccc7ffd2dc3f158b178a2a5bcfb
Sean Massa <endangeredmassa@gmail.com>
2024-02-01 22:10:17 -0600

    analytics

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { run, scheduleOnce } from '@ember/runloop';
 import hljs from '../common/highlight.min';
+import { inject as registerVercelAnalytics } from '@vercel/analytics';
 
 let firstLoad = true;
 
@@ -10,6 +11,10 @@ export default class ApplicationRoute extends Route {
   @service router;
 
   activate() {
+    if (firstLoad && !this.fastboot.isFastBoot) {
+      registerVercelAnalytics();
+    }
+
     this.router.on('routeDidChange', (transition) => {
       if (this.fastboot.isFastBoot) {
         return;
